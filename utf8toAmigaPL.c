eeb8c55635e48cee81bc276afff7aed0c3e20a70
Rafał Zabdyr <rafal.zabdyr@tdolphin.org>
2022-02-12 10:50:58 +0100

    update conversion

--- utf8toAmigaPL.c
+++ utf8toAmigaPL.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 
-#define AMIGAPL_A   194
-#define AMIGAPL_C   202
-#define AMIGAPL_E   203
-#define AMIGAPL_L   206
-#define AMIGAPL_N   207
-#define AMIGAPL_O   211
-#define AMIGAPL_S   212
-#define AMIGAPL_X   218
-#define AMIGAPL_Z   219
-#define AMIGAPL_a   226
-#define AMIGAPL_c   234
-#define AMIGAPL_e   235
-#define AMIGAPL_l   238
-#define AMIGAPL_n   239
-#define AMIGAPL_o   243
-#define AMIGAPL_s   244
-#define AMIGAPL_x   250
-#define AMIGAPL_z   251
+#define AMIGAPL_A 194
+#define AMIGAPL_C 202
+#define AMIGAPL_E 203
+#define AMIGAPL_L 206
+#define AMIGAPL_N 207
+#define AMIGAPL_O 211
+#define AMIGAPL_S 212
+#define AMIGAPL_X 218
+#define AMIGAPL_Z 219
+#define AMIGAPL_a 226
+#define AMIGAPL_c 234
+#define AMIGAPL_e 235
+#define AMIGAPL_l 238
+#define AMIGAPL_n 239
+#define AMIGAPL_o 243
+#define AMIGAPL_s 244
+#define AMIGAPL_x 250
+#define AMIGAPL_z 251
 
-#define UTF8_A  0x84
-#define UTF8_C  0x86
-#define UTF8_E  0x98
-#define UTF8_L  0x81
-#define UTF8_N  0x83
-#define UTF8_O  0x93
-#define UTF8_S  0x9A
-#define UTF8_X  0xB9
-#define UTF8_Z  0xBB
-#define UTF8_a  0x85
-#define UTF8_c  0x87
-#define UTF8_e  0x99
-#define UTF8_l  0x82
-#define UTF8_n  0x84
-#define UTF8_o  0xB3
-#define UTF8_s  0x9B
-#define UTF8_x  0xBA
-#define UTF8_z  0xBC
+#define AMIGA_DOUBLE_QUOTATION_MARK 0x22
+
+#define UTF8_A 0x84
+#define UTF8_C 0x86
+#define UTF8_E 0x98
+#define UTF8_L 0x81
+#define UTF8_N 0x83
+#define UTF8_O 0x93
+#define UTF8_S 0x9A
+#define UTF8_X 0xB9
+#define UTF8_Z 0xBB
+#define UTF8_a 0x85
+#define UTF8_c 0x87
+#define UTF8_e 0x99
+#define UTF8_l 0x82
+#define UTF8_n 0x84
+#define UTF8_o 0xB3
+#define UTF8_s 0x9B
+#define UTF8_x 0xBA
+#define UTF8_z 0xBC
+
+#define UTF8_LEFT_DOUBLE_QUOTATION_MARK 0x9C
+#define UTF8_RIGHT_DOUBLE_QUOTATION_MARK 0x9E
 
 
 int main(int argc, char *argv[])
@@ -44,7 +49,7 @@ int main(int argc, char *argv[])
     if (argc != 3)
     {
         printf("Wrong number of parameters!\n");
-        printf("usage:\n %s UTF8-txt-input-file amigaPL-txt-outputfile\n", __PRG_FILE__);
+        printf("usage:\n %s UTF8txtInputFile AmigaPLtxtOutputFile\n", __PRG_FILE__);
         return 0;
     }
 
@@ -139,6 +144,25 @@ int main(int argc, char *argv[])
                             case UTF8_x: // ź
                                 fputc(AMIGAPL_x, fileOut);
                                 break;
+                        }
+                        break;
+                    case 0xE2:
+                        oneChar = fgetc(fileIn);
+                        if (feof(fileIn))
+                            break;
+                        switch(oneChar)
+                        {
+                            case 0x80:
+                                oneChar = fgetc(fileIn);
+                                if (feof(fileIn))
+                                    break;
+                                switch(oneChar)
+                                {
+                                    case UTF8_LEFT_DOUBLE_QUOTATION_MARK:
+                                    case UTF8_RIGHT_DOUBLE_QUOTATION_MARK:
+                                        fputc(AMIGA_DOUBLE_QUOTATION_MARK, fileOut);
+                                        break;
+                                }
                         }
                         break;
                     default:
